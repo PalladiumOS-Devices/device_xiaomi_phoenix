d21eeec068e5ea9e7d64e4459006e7c5d1688a83
Chiranth A J <chiranth@m.ms.evolution-x.org>
2022-03-22 23:06:26 +0530

    Bring Up PalladiumOS
    Signed-off-by: Chiranth A J <chiranth@m.ms.evolution-x.org>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_phoenix.mk
+    $(LOCAL_DIR)/palladium_phoenix.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_phoenix-user \
-    aosp_phoenix-userdebug \
-    aosp_phoenix-eng
+    palladium_phoenix-user \
+    palladium_phoenix-userdebug \
+    palladium_phoenix-eng
--- palladium_phoenix.mk
+++ palladium_phoenix.mk
@@ -7,10 +7,10 @@
 $(call inherit-product, device/xiaomi/phoenix/device.mk)
 
 # Inherit some common Custom stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/palladium/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := aosp_phoenix
+PRODUCT_NAME := palladium_phoenix
 PRODUCT_DEVICE := phoenix
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := POCO X2
@@ -19,6 +19,10 @@ PRODUCT_MANUFACTURER := Xiaomi
 # Custom Properties
 TARGET_GAPPS_ARCH := arm64
 TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_SUPPORTS_QUICK_TAP := true
+
+# Official-build-only
+PALLADIUM_BUILD_TYPE := OFFICIAL
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.palladium.maintainer=Chiranth
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
